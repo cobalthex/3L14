6d9a917ff306e7d91fb45489f27e4c86d86d1b0d
CobaltHex <CobaltHex@live.com>
2025-02-23 23:54:31 -0800

    Cargo tweaks + windows manifewt file

--- src/build/build_main.rs
+++ src/build/build_main.rs
@@ -11,6 +11,18 @@ fn main()
     if env::var_os("CARGO_CFG_WINDOWS").is_some()
     {
         WindowsResource::new()
+        .set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3" manifestVersion="1.0">
+    <asmv3:application>
+        <asmv3:windowsSettings>
+            <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>
+            <longPathAware xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">true</longPathAware>
+            <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true</dpiAware>
+            <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+        </asmv3:windowsSettings>
+    </asmv3:application>
+</assembly>
+        "#)
         .set_icon("res/App.ico")
             .compile().expect("! Failed to compile windows resource definitions");
     }
